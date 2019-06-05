dfd740a3c1e29b8e00d4033ab2fb95fe117af7af
Kelsey Low <kklowlow@gmail.com>
2019-06-05 21:57:02 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,30 @@
 class GenresController < ApplicationController
+  def show 
+    @genre = Genre.find(params[:id])
+  end
   
+  def new 
+    @genre = Genre.new
+  end
+  
+  def create 
+    @genre = Genre.create(genre_params(:name))
+    redirect_to genres_path(@genre)
+  end
+  
+  def edit 
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update 
+    @genre = Genre.find(params[:id])
+    @genre = Genre.update(genre_params(:name))
+    redirect_to genres_path(@genre)
+  end
+  
+  private 
+  
+  def artist_params(*args) 
+    params.require(:genre).permit(*args)
+  end
 end
