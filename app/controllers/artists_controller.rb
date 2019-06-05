cf1db1f3c752140dced740cf5dbb29a402fba0d0
Kelsey Low <kklowlow@gmail.com>
2019-06-05 21:46:13 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,10 @@
 class ArtistsController < ApplicationController
   def show 
-    
+    @artist = Artist.find(params[:id])
   end
   
   def new 
-    
+    @artist = Artist.new
   end
   
   def create 
